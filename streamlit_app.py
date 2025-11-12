c8e59cea254a3fe091f12bb1c6a19e00e677ec95
EslamAlsabbagh <49647683+EslamAlsabbagh@users.noreply.github.com>
2025-11-13 00:45:21 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,10 +3,10 @@ import bridge
 
 st.title("ZKTeco Bridge Control")
 
-if st.button("Run Bridge"):
     st.write("Bridge started...")
     try:
         bridge.main()
         st.success("Bridge executed successfully!")
     except Exception as e:
         st.error(f"Error: {e}")
+
